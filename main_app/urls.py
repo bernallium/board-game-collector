2614b75d9e9312e1365cf8371afb3ccdbab9ea85
Brandon Bernal <bernal.brandon@gmail.com>
2020-04-08 00:21:06 -0400

    Add game details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, about, games_index
+from .views import home, about, games_index, games_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('games/', games_index, name='index'),
+    path('games/<int:game_id>/', games_detail, name='detail'),
 ]
