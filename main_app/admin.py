cc53ab11757ab53fc1b6189bd63cdb9af7f23e59
Brandon Bernal <bernal.brandon@gmail.com>
2020-04-07 23:40:21 -0400

    Add models, index pages and routes, and admin functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import your models
+from .models import Game
+
+# Register your models
+admin.site.register(Game)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+    
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True)
+    category = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(null=True)
+    min_players = models.PositiveIntegerField(null=True)
+    max_players = models.PositiveIntegerField(null=True)
+    description = models.TextField(max_length=250)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
