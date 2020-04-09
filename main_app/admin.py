ac457deef9d5bd9fbec20efe0a009c35b4089033
Brandon Bernal <bernal.brandon@gmail.com>
2020-04-08 23:45:21 -0400

    Fix photonot uploading issue

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Import your models
-from .models import Game
-from .models import Session
+from .models import Game, Session, Photo
 
 # Register your models
 admin.site.register(Game)
 admin.site.register(Session)
+admin.site.register(Photo)
