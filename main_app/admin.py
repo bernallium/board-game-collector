ed7904240838cafb9420859700fa0e52ced4d4b2
Brandon Bernal <bernal.brandon@gmail.com>
2020-04-10 01:29:12 -0400

    Add Label model to Admin app

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Import your models
-from .models import Game, Session, Photo
+from .models import Game, Session, Label, Photo
 
 # Register your models
 admin.site.register(Game)
 admin.site.register(Session)
+admin.site.register(Label)
 admin.site.register(Photo)
